1758f32ae0baa535db08a63525bc8b38cf2e9018
AntonyKLINGER <klingerantony@gmail.com>
2023-10-24 09:18:24 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from 'next/head'
+import '@/styles/responsify.css'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;600;700;800&display=swap" rel="stylesheet" />        
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
